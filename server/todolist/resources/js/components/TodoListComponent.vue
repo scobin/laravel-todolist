caf45bd18c9cef3df15f426dcfce12ba34ec22b3
hobrunt <hobrunt.dev@cyublog.com>
2021-12-04 03:53:44 +0000

    implement completed and deleteTodo methods.

--- server/todolist/resources/js/components/TodoListComponent.vue
+++ server/todolist/resources/js/components/TodoListComponent.vue
@@ -8,12 +8,21 @@
         <ul>
             <li v-for="(todo, index) in todos" :key="index">
                 <label :for="`todo-${index}`">
-                    <span>{{ todo.body }}</span>
+                    <span :class="[todo.completed ? 'line-through' : '']" >{{ todo.body }}</span>
                 </label>
+                <div>
+                    <button @click="completed(todo)"> O </button>
+                    <button @click="deleteTodo(todo)"> - </button>
+                </div>
             </li>
         </ul>
     </div>
 </template>
+<style>
+    .line-through {
+        text-decoration: line-through;
+    }
+</style>
 <script>
     export default {
         data: function() {
@@ -53,6 +62,30 @@
                         // clear input
                         this.newTodo.body = ''
                         this.todos.unshift(res.data)
+                    }
+                }).catch( error => {
+                    console.log(error)
+                })
+            },
+            completed(todo) {
+                axios.put(`api/todo/${todo.id}`, {
+                    completed: !todo.completed
+                }).then( res => {
+                    console.log(res)
+                    if (res.status == 200) {
+                        // update todo
+                        todo.completed = !todo.completed
+                    }
+                }).catch( error => {
+                    console.log(error)
+                })
+            },
+            deleteTodo(todo) {
+                axios.delete(`api/todo/${todo.id}`)
+                .then( res => {
+                    console.log(res)
+                    if (res.status == 204) {
+                        this.todos = this.todos.filter(item => item != todo)
                     }
                 }).catch( error => {
                     console.log(error)
